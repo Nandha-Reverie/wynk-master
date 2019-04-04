87adbccb2dd5851f1a7a0ed107d003f975890d4a
NandhaKumar <reverie-pc@Reverie-PCs-MacBook-Pro.local>
2019-04-04 15:17:43 +0530

    initial commit

---
+++ build/precache-manifest.40fb37ed7ee6ae045d52d2abdf888a08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "efdf9b1ef9fc2e84a64d",
+    "url": "/static/js/main.93a83927.chunk.js"
+  },
+  {
+    "revision": "accbbb2cc9fb09b4aba3",
+    "url": "/static/js/2.c5b9d6d3.chunk.js"
+  },
+  {
+    "revision": "efdf9b1ef9fc2e84a64d",
+    "url": "/static/css/main.4b84b857.chunk.css"
+  },
+  {
+    "revision": "accbbb2cc9fb09b4aba3",
+    "url": "/static/css/2.48204023.chunk.css"
+  },
+  {
+    "revision": "fb242de473e66ae654f919651e96d29b",
+    "url": "/index.html"
+  }
+];
